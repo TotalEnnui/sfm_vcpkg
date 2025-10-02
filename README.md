9230e7ce1f42aa2ea8c4945acc2303c3049f942a
Adrian Poston <aprpkp@gmail.com>
2025-10-02 18:59:09 -0400

    Updated everything 251002

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-README.md
+# README.md
 
-compile: 
-    cmake --preset linux-release
-    cmake --build --preset linux-release
+## ucrt64 bash   
+  
+```rm -rf build/ucrt64-gcc```   
+```cmake --preset ucrt64-gcc```   
+```cmake --build --preset ucrt64-gcc```   
 
-delete build directory:
-    
+
+## install clarification
+vs code taskbar configuration only does configurations and build, NOT intall.  
+Install must be initiated via terminal, bash (gcc) or developer powershell (MSVC).  
+Outputs just outputs but install installs, in order to maintain separtion  
+
+### gcc install, ucrt64 bash
+```cmake --install build/ucrt64-gcc```  
+### MVSC intall, PowerShell     
+```cmake --install build/release --config Release```  
+
---
+++ out/build/MSVC-debug/cmake_install.cmake
@@ -0,0 +1,56 @@
+# Install script for directory: C:/Users/alp19/Documents/repos/sfm_vcpkg
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Users/alp19/Documents/repos/sfm_vcpkg/install/debug")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/alp19/Documents/repos/sfm_vcpkg/debug/Debug/main.exe")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/alp19/Documents/repos/sfm_vcpkg/debug/Release/main.exe")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/alp19/Documents/repos/sfm_vcpkg/debug/MinSizeRel/main.exe")
+  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/alp19/Documents/repos/sfm_vcpkg/debug/RelWithDebInfo/main.exe")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "C:/Users/alp19/Documents/repos/sfm_vcpkg/out/build/MSVC-debug/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
